7877045279b3b410a7f58bdb2c5c49c7d7d2c386
Woinkk <Darkrai99@live.fr>
2023-04-22 18:52:49 +0200

    Add child routes for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { SchoolPathComponent } from './components/school-path/school-path.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    component: HomePageComponent
+    component: HomePageComponent,
+    children: [
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'career',
+        component: SchoolPathComponent
+      },
+    ]
   },
   {
     path: '**',
