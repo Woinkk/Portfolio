54ea62e5165b349644ae8f77ce5d1fae0d0f9183
Woinkk <Darkrai99@live.fr>
2023-04-22 18:52:35 +0200

    Display home layout when navigation is /home

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, UrlSegment } from '@angular/router';
 
 @Component({
   templateUrl: './home-page.component.html',
@@ -6,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  currentRoute: UrlSegment[] = [];
+
+  constructor(
+    private _router: Router
+  ) { 
+    let segments = this._router.getCurrentNavigation()?.finalUrl?.root.children['primary'].segments;
+    if(segments) {
+      this.currentRoute = segments;
+      console.log(this.currentRoute.length);
+      
+    }
+  };
 
   ngOnInit(): void {
+    this._router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        let segments = this._router.getCurrentNavigation()?.finalUrl?.root.children['primary'].segments;
+        if(segments) {
+          this.currentRoute = segments;
+        }
+      }
+    })
   }
 
 }
