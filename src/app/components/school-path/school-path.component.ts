50b79a6ef2ef10442f7e0ad066872ac2745d2b80
Woinkk <Darkrai99@live.fr>
2023-05-09 07:13:49 +0200

    WIP timeline on school component

--- src/app/components/school-path/school-path.component.ts
+++ src/app/components/school-path/school-path.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolPathComponent implements OnInit {
 
+  timeline: any[] = [
+    {
+      year: '2018',
+      title: 'Titre',
+      details: 'Détails'
+    },
+    {
+      year: '2018 - 2020',
+      title: 'Titre',
+      details: 'Détails'
+    },
+    {
+      year: '2020 - 2022',
+      title: 'Titre',
+      details: 'Détails'
+    },
+    {
+      year: '2022 - Maintenant',
+      title: 'Titre',
+      details: 'Détails'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
