9bbbd8b1200a4a3bf30feb979638c931585a05b1
Woinkk <Darkrai99@live.fr>
2023-05-14 17:31:51 +0200

    add article service

--- src/app/shared/service/article.service.ts
+++ src/app/shared/service/article.service.ts
@@ -8,12 +8,18 @@ import { Article } from '../models/article.model';
 })
 export class ArticleService {
   
-  private _articleSubject: Subject<Article> = new BehaviorSubject<Article>({} as Article);
-  public articleObservable$: Observable<Article> = new Observable<Article>();
+  private _articleSubject: Subject<Article | null> = new BehaviorSubject<Article | null>(null);
+  public articleObservable$: Observable<Article | null>;
+
+  private _skillSubject: Subject<Article | null> = new BehaviorSubject<Article | null>(null);
+  public skillObservable$: Observable<Article | null>;
 
   constructor(
     private _httpClient: HttpClient
-  ) { }
+  ) { 
+    this.articleObservable$ = this._articleSubject.asObservable();
+    this.skillObservable$ = this._skillSubject.asObservable();
+  }
 
 
   public setArticle(articleName: string): void {
@@ -21,9 +27,30 @@ export class ArticleService {
   }
 
   private setArticleSubject(articleName: string): void {
-    const url = `/assets/articles/${articleName}/content.html`
-    this._httpClient.get<Article>(url).subscribe(article => {
+    const url = `./assets/articles/${articleName}/content.json`
+    this._httpClient.get<Article>(url).subscribe((article) => {
      this._articleSubject.next(article);
     });
+  }
+
+  public unsetArticle(): void {
+    this._articleSubject.next(null);
+  }
+
+  public setSkill(skillName: string): void {
+    console.log(skillName);
+    
+    this.setSkillSubject(skillName);
+  }
+
+  setSkillSubject(skillName: string) {
+    const url = `./assets/articles/${skillName}/content.json`
+    this._httpClient.get<Article>(url).subscribe((skill) => {
+     this._skillSubject.next(skill);
+    });
+  }
+
+  public unsetSkill(): void {
+    this._skillSubject.next(null);
   }
 }
