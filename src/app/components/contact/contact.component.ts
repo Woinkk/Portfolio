77ffb41cbc53aa23a6fb8b172fee75f5572ddc0e
Woinkk <Darkrai99@live.fr>
2023-07-02 02:57:26 +0200

    Add socials on contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SnackBarComponent } from '../snack-bar/snack-bar.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   templateUrl: './contact.component.html',
@@ -9,29 +12,40 @@ import { SnackBarComponent } from '../snack-bar/snack-bar.component';
 export class ContactComponent implements OnInit {
 
   firstName: string = "";
-  lastName: string = "";
+  mail: string = "";
   subject: string = "";
   message: string = "";
+  linkedinIcon: string = "E:\dev\Perso\Woinkk.github.io\src\assets\icons8-linkedin.svg";
 
   constructor(
-    private _snackBar: MatSnackBar
-  ) { }
+    @Inject(DOCUMENT) private document: Document,
+    private _snackBar: MatSnackBar,
+    private iconRegistry: MatIconRegistry, 
+    private sanitizer: DomSanitizer
+  ) { 
+    this.iconRegistry.addSvgIcon('linkedin', this.sanitizer.bypassSecurityTrustResourceUrl('../../../assets/linkedinIcon.svg'));
+  }
 
   ngOnInit(): void {
+    
   }
 
   openSnackBar() {
-    if( this.firstName == "" || this.lastName == "" || this.subject == "" || this.message == "") {
+    if( this.firstName == "" || this.mail == "" || this.subject == "" || this.message == "") {
       this._snackBar.open("Veuillez remplir tout les champs de contact.", "Fermer")
     } else {
       this._snackBar.openFromComponent(SnackBarComponent, {
         duration: 5 * 1000, // 5 seconds
       });
       this.firstName = "";
-      this.lastName = "";
+      this.mail = "";
       this.subject = "";
       this.message = "";
     }
   }
+
+  goToUrl(): void {
+    this.document.location.href = 'https://www.linkedin.com/in/spencer-galloni/';
+}
 
 }
